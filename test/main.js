138c7b335164ac5d281d86cbef6e37f672fc2033
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2021-04-14 20:53:48 +0200

    npm publish time <3

--- test/main.js
+++ test/main.js
@@ -2,20 +2,22 @@ import './style.css';
 
 import { Veifa, LineRenderer } from '../dist';
 
-const cavnas = document.getElementById('canvas');
-const veifa = new Veifa(cavnas);
+window.onload = async () => {
+	const cavnas = document.getElementById('canvas');
+	const veifa = new Veifa(cavnas);
 
-const src = 'https://publicshitbucket.s3.eu-central-1.amazonaws.com/115.mp3';
-await veifa.loadSrc(src);
+	const src = 'https://publicshitbucket.s3.eu-central-1.amazonaws.com/115.mp3';
+	await veifa.loadSrc(src);
 
-veifa.renderer = new LineRenderer();
-veifa.render();
+	veifa.renderer = new LineRenderer();
+	veifa.render();
 
-const toggleButton = document.getElementById('toggle');
-toggleButton.onclick = () => veifa.toggle();
+	const toggleButton = document.getElementById('toggle');
+	toggleButton.onclick = () => veifa.toggle();
 
-const playButton = document.getElementById('play');
-playButton.onclick = () => veifa.play();
+	const playButton = document.getElementById('play');
+	playButton.onclick = () => veifa.play();
 
-const pauseButton = document.getElementById('pause');
-pauseButton.onclick = () => veifa.pause();
+	const pauseButton = document.getElementById('pause');
+	pauseButton.onclick = () => veifa.pause();
+};
